369ec8bc8bb4813ea4dccc5d73f5056d72ebdc15
Sharfuddin786 <53270668+Sharfuddin786@users.noreply.github.com>
2019-08-23 22:58:56 +0530

    Exercise 4 : Astrologer's Stars

---
+++ Astrologer's Stars.py
@@ -0,0 +1,10 @@
+inp_int = int(input("Enter a number: \n"))
+inp_bool = int(input('Enter 0/1 : \n'))
+i = 0
+while i<inp_int:
+    if inp_bool== True:
+        print("*"*i)
+        i+=1
+    elif inp_bool== False:
+        print("*"*inp_int)
+        inp_int-=1
