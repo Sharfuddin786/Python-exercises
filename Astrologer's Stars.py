3002c710e58a789efc79e808b90768bf2a455bd2
Sharfuddin786 <53270668+Sharfuddin786@users.noreply.github.com>
2019-08-30 14:57:57 +0530

    Update Astrologer's Stars.py

--- Astrologer's Stars.py
+++ Astrologer's Stars.py
@@ -3,7 +3,7 @@ inp_bool = int(input('Enter 0/1 : \n'))
 i = 0
 while i<inp_int:
     if inp_bool== True:
-        print("*"*i)
+        print("*"*(i+1))
         i+=1
     elif inp_bool== False:
         print("*"*inp_int)
